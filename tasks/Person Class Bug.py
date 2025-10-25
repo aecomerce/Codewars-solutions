0c65075933bac3ffc0d341f4268660f004317b0d
Arkadiy Fesler <arkadijfesler5@gmail.com>
2025-10-25 21:02:11 +0500

    Create Person Class Bug.py

---
+++ tasks/Person Class Bug.py
@@ -0,0 +1,7 @@
+class Person():
+
+    def __init__(self, first_name: str, last_name: str, age: int):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.full_name = self.first_name + ' ' + self.last_name
